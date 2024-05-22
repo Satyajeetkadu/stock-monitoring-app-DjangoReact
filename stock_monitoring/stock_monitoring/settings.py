879b6767d9a63204879577392694bb09c6cd1b02
Satyajeet Kadu <satyajeetkadu@Satyajeets-MacBook-Pro.local>
2024-05-22 18:48:53 +0530

    Initial commit with frontend and backend

--- stock_monitoring/stock_monitoring/settings.py
+++ stock_monitoring/stock_monitoring/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-kbz9$&-wf^(+%)s4^8m&yu1&jf!^u25xnek3s%^wk-d7gi%%$6')
+SECRET_KEY = 'django-insecure-kbz9$&-wf^(+%)s4^8m&yu1&jf!^u25xnek3s%^wk-d7gi%%$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']  # Allow all hosts for now, you can specify your domain later
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'watchlist',
     'rest_framework',
     'rest_framework_simplejwt',
+
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -51,7 +52,6 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,7 +85,10 @@ WSGI_APPLICATION = 'stock_monitoring.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3', conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -124,16 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# Enable WhiteNoise for serving static files in production
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# Security settings for Heroku
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- stock_monitoring/watchlist/management/commands/fetch_stock_details.py
+++ stock_monitoring/watchlist/management/commands/fetch_stock_details.py
@@ -1,34 +1,34 @@
-import requests
-from django.core.management.base import BaseCommand
-from django.utils import timezone
-from watchlist.models import StockSymbol
+# import requests
+# from django.core.management.base import BaseCommand
+# from django.utils import timezone
+# from watchlist.models import StockSymbol
 
-class Command(BaseCommand):
-    help = 'Fetch stock details from Alpha Vantage API'
+# class Command(BaseCommand):
+#     help = 'Fetch stock details from Alpha Vantage API'
 
-    def handle(self, *args, **kwargs):
-        api_key = 'N7GGKDLXF6M37WP0'
-        base_url = 'https://www.alphavantage.co/query'
+#     def handle(self, *args, **kwargs):
+#         api_key = 'N7GGKDLXF6M37WP0'
+#         base_url = 'https://www.alphavantage.co/query'
 
-        for stock in StockSymbol.objects.all():
-            params = {
-                'function': 'TIME_SERIES_INTRADAY',
-                'symbol': stock.symbol,
-                'interval': '1min',
-                'apikey': api_key
-            }
-            response = requests.get(base_url, params=params)
-            data = response.json()
+#         for stock in StockSymbol.objects.all():
+#             params = {
+#                 'function': 'TIME_SERIES_INTRADAY',
+#                 'symbol': stock.symbol,
+#                 'interval': '1min',
+#                 'apikey': api_key
+#             }
+#             response = requests.get(base_url, params=params)
+#             data = response.json()
 
-            try:
-                time_series = data['Time Series (1min)']
-                latest_time = sorted(time_series.keys())[0]
-                latest_data = time_series[latest_time]
+#             try:
+#                 time_series = data['Time Series (1min)']
+#                 latest_time = sorted(time_series.keys())[0]
+#                 latest_data = time_series[latest_time]
 
-                stock.last_price = latest_data['4. close']
-                stock.last_updated = timezone.now()
-                stock.save()
+#                 stock.last_price = latest_data['4. close']
+#                 stock.last_updated = timezone.now()
+#                 stock.save()
 
-                self.stdout.write(self.style.SUCCESS(f'Successfully updated {stock.symbol}'))
-            except KeyError:
-                self.stdout.write(self.style.ERROR(f'Failed to fetch data for {stock.symbol}'))
+#                 self.stdout.write(self.style.SUCCESS(f'Successfully updated {stock.symbol}'))
+#             except KeyError:
+#                 self.stdout.write(self.style.ERROR(f'Failed to fetch data for {stock.symbol}'))
